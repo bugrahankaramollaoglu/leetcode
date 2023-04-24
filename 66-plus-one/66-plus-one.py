770b59dab0c2029da23948b60150a38836d21523
buğrahan karamollaoğlu <85173065+bugrahankaramollaoglu@users.noreply.github.com>
2023-04-24 16:46:38 +0300

    Time: 30 ms (84.46%), Space: 13.9 MB (44.05%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        nb = self.list_to_int(digits)
+        nb += 1
+        return (self.int_to_list(nb))
+
+    def list_to_int(self, n):
+        num = 0
+        for i in n:
+            num = i + (num * 10)
+        return num
+
+    def int_to_list(self, n):
+        nbList = list()
+        while (n):
+            nbList.append(n % 10)
+            n //= 10
+        nbList.reverse()
+        return nbList
