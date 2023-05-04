529a88284552ded2a67bc5239440fd5ffa4c58bf
buğrahan karamollaoğlu <85173065+bugrahankaramollaoglu@users.noreply.github.com>
2023-05-05 01:10:53 +0300

    Time: 6 ms (56.33%), Space: 6.1 MB (45.00%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *restoreString(char *s, int *indices, int indicesSize)
+{
+	char *s2 = malloc(sizeof(char) * strlen(s) + 1);
+	int i = 0;
+	int i2 = 0;
+	if (!s2)
+		return 0;
+	for (i = 0; i < strlen(s); i++)
+	{
+		s2[indices[i]] = s[i];
+	}
+	s2[i] = '\0';
+	return s2;
+}
