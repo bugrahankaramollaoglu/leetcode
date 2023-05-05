84aca968231fdf39db4e93752e96cfd11852478c
buğrahan karamollaoğlu <85173065+bugrahankaramollaoglu@users.noreply.github.com>
2023-05-05 16:02:32 +0300

    Time: 4 ms (84.89%), Space: 6.7 MB (79.58%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -0,0 +1,12 @@
+int *runningSum(int *nums, int numsSize, int *returnSize)
+{
+	int *nums2 = malloc(sizeof(int) * numsSize);
+	*returnSize = numsSize;
+	int sum = 0;
+	for (int i = 0; i < numsSize; i++) {
+		for (int i2=i; i2 < i+1; i2++)
+			sum += nums[i2];
+		nums2[i] = sum;
+	}
+	return nums2;
+}
