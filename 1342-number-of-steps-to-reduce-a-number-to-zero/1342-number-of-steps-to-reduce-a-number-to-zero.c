a306f25ba899b8aa13429beb57a2d9da17ac4d48
buğrahan karamollaoğlu <85173065+bugrahankaramollaoglu@users.noreply.github.com>
2023-05-07 21:16:25 +0300

    Time: 4 ms (27.55%), Space: 5.6 MB (17.21%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,11 @@
+int numberOfSteps(int num){
+    int step = 0;
+    while (num) {
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num -= 1;
+        step++;
+    }
+    return step;
+}
